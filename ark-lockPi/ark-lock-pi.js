c8cd0ccbf9861f32f250492659d11a90964b71c7
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-22 18:46:14 -0400

    Update ark-lock-pi.js
    Expand logic and flow.
    add Pushbutton test function.

--- ark-lockPi/ark-lock-pi.js
+++ ark-lockPi/ark-lock-pi.js
@@ -1,25 +1,20 @@
 #!/usr/bin/env node
 
 var arkjs = require("arkjs");
+var events = require('events');
 var fs = require('fs');
 var vorpal = require('vorpal')();
-
-/* === uncomment 'rpio' for live-device testing === */
 // var rpio = require('rpio');
-
 
 /* FIXME: not safe. Need to consolidate and encrypt data. */
 const ACCOUNT_FILE = 'tmp/address.txt';
 const LID_FILE = 'tmp/lid.txt';
 const PRICE_FILE = 'tmp/price.txt';
 
-
 module.exports = function (vorpal) {
-
 
 /* ============================================ */
 /* ========= Set and Get Lock Address ========= */
-
 
 /* ==== */
 /* Set */
@@ -50,7 +45,6 @@ vorpal
     });
   });
 
-
 /* ==== */
 /* Get */
 vorpal
@@ -69,7 +63,6 @@ vorpal
     callback();
   });
 
-
 /* ============================================ */
 /* ============================================ */
 
@@ -77,10 +70,8 @@ vorpal
 /* ============================================ */
 /* ======= Set Lock LocationID & Price ======= */
 
-
 /* ============ */
 /* setLock lid */
-
 
 vorpal
   .command('setLid')
@@ -109,10 +100,8 @@ vorpal
     });
   });
 
-
 /* ============== */
 /* setLock Price */
-
 vorpal
   .command('setPrice')
   .action(function(args, callback) {
@@ -144,17 +133,12 @@ vorpal
   /* ============================================ */
 
 
-
-
   /* ============================================ */
   /* ========= Get Unlock Info ========= */
-
 
   vorpal
     .command('getLockInfo', 'Read your address, lid, & price from file')
     .action(function(args, callback, err) {
-
-
       if (!err) {
         /* ===== Address ===== */
         var address = '';
@@ -177,7 +161,6 @@ vorpal
         /* TODO: Add ItemID */
         /* ===== ItemID ===== */
 
-
         /* ===== Price ===== */
         var price = '';
         readStream = fs.createReadStream(PRICE_FILE, 'utf8');
@@ -196,15 +179,42 @@ vorpal
 
 
     /* ============================================ */
-    /* ========= Create Smartbridge Data. ========= */
-
-    /* ============================================ */
-    /* ============================================ */
-
-
-    /* ============================================ */
     /* ============== Button Pushed. ============== */
-    /* ================= Tell NFC ================= */
+  // detect button push GPIO
+
+    vorpal
+      .command('pushButton', 'Simulates Button Push')
+      .action(function() {
+        console.log("Simulating Button Push\n");
+
+        console.log("Starting Button Test\n")
+
+        buttonPush.on('buttonWasPushed', buttonWasPushed);
+        buttonPush.emit('buttonWasPushed');
+      });
+
+    // rpio.open(00, rpio.INPUT, rpio.PULL_DOWN);
+    // rpio.poll(1, buttonPush);
+    // function buttonPush(pin){
+    // 	// var state = rpio.read(pin) ? 'pressed' : 'notPressed';
+    // 	// if (state == 'pressed') {
+    // 		// makeTX
+    //     nfcData.emit('dataNeedsMade');
+    // 	// }
+    // }
+
+    var buttonPush = new events.EventEmitter();
+
+    var buttonWasPushed = function () {
+      console.log('**** buttonWasPushed ****');
+      console.log("Getting ready to make TX Requests...\n");
+
+      //make data for NFC
+
+      // if data was presented & scanned
+      nfcData.on('dataNeedsMade', dataNeedsMade);
+      nfcData.emit('dataNeedsMade');
+    };
 
     /* ============================================ */
     /* ============================================ */
@@ -212,6 +222,21 @@ vorpal
 
     /* ============================================ */
     /* =========== Make NFC TX Request. =========== */
+		/* ========= Create Smartbridge Data. ========= */
+    // use lid & prices & other to make smartbridge data
+
+    var nfcData = new events.EventEmitter();
+
+    var dataNeedsMade = function () {
+      console.log('**** dataNeedsMade ****');
+      console.log("Making NFC data...\n");
+
+      //make data for NFC
+
+      // if data was presented & scanned
+      nfcRequest.on('nfcWasRequested', nfcWasRequested);
+      nfcRequest.emit('nfcWasRequested');
+    }
 
     /* ============================================ */
     /* ============================================ */
@@ -219,34 +244,100 @@ vorpal
 
     /* ============================================ */
     /* ========== Present NFC TX Request ========== */
+    // wake NFC GPIO
+    // present tx request via nfc
+    //rpio.open(01, rpio.INPUT, rpio.PULL_DOWN);
 
+    var nfcRequest = new events.EventEmitter();
+
+    var nfcWasRequested = function () {
+      console.log('**** nfcWasRequested ****');
+      console.log("Presenting data via NFC...\n")
+
+      //present data via NFC
+
+      // if data was presented & scanned
+      nfcScanned.on('nfcWasScanned', nfcWasScanned);
+      nfcScanned.emit('nfcWasScanned');
+    }
     /* ============================================ */
     /* ============================================ */
 
 
     /* ============================================ */
     /* ========== NFC TX Acknowledgement ========== */
+    // confirm that NFC data was scanned
+    // nfc sleep after nfc scanned
+    // triggers check for TX cycle
+
+    var nfcScanned = new events.EventEmitter();
+
+    var nfcWasScanned = function () {
+      console.log('**** nfcWasScanned ****');
+      console.log("Confirming NFC was scanned...\n");
+
+      // if TX valid
+      checkTX.on('shouldCheckTX', shouldCheckTX);
+      checkTX.emit('shouldCheckTX');
+    }
+    /* ============================================ */
+    /* ============================================ */
+
+
+    /* ============================================ */
+    /* =============== Check for TX =============== */
+    // start checking for tx on mainnet
+    // check every X
+
+    var checkTX = new events.EventEmitter();
+
+    var shouldCheckTX = function () {
+      console.log('**** shouldCheckTX ****');
+      console.log("Checking for TX...\n")
+
+      // if TX valid
+      txReceived.on('txWasReceived', txWasReceived)
+      txReceived.emit('txWasReceived');
+    }
 
     /* ============================================ */
     /* ============================================ */
 
 
     /* ============================================ */
-    /* ============== Check for TX ================ */
+    /* =============== Validate TX. =============== */
+    // Check found TX for validity before approving unlock
+
+    var txReceived = new events.EventEmitter();
+
+    var txWasReceived = function () {
+      console.log('**** txWasReceived ****');
+      console.log("Validating...\n")
+
+      // if TX valid
+      unlock.on('shouldUnlock', shouldUnlock);
+      unlock.emit('shouldUnlock');
+    }
 
     /* ============================================ */
     /* ============================================ */
 
 
     /* ============================================ */
-    /* ============== Validate TX. ================ */
+    /* ================== Unlock ================== */
+    // do unlock on pins
+    //rpio.open(02, rpio.INPUT, rpio.PULL_DOWN);
 
-    /* ============================================ */
-    /* ============================================ *
+    var unlock = new events.EventEmitter();
 
+    var shouldUnlock = function () {
+      console.log('**** shouldUnlock ****');
+      console.log('Unlocking...\n');
 
-    /* ============================================ */
-    /* ================ Unlock ==================== */
+      //if was unlocked
+      unlocked.on('wasUnlocked', wasUnlocked);
+      unlocked.emit('wasUnlocked');
+    }
 
     /* ============================================ */
     /* ============================================ */
@@ -254,21 +345,42 @@ vorpal
 
     /* ============================================ */
     /* ============== Verify Unlock. ============== */
+    // check if unlock was successful on pins
 
+    var unlocked = new events.EventEmitter();
+
+    var wasUnlocked = function () {
+      console.log('**** wasUnlocked ****');
+      console.log('Verifying...\n');
+
+      // if unlock was verified
+      saveTX.on('shouldSaveTX', shouldSaveTX);
+      saveTX.emit('shouldSaveTX');
+    }
     /* ============================================ */
     /* ============================================ */
 
 
     /* ============================================ */
     /* =============== Save TX Info =============== */
-
-    /* ============================================ */
-    /* ============================================ */
-
+    //  save TX to file
     
+    var saveTX = new events.EventEmitter();
+
+    var shouldSaveTX = function (callback) {
+      console.log('**** shouldSaveTX ****');
+      console.log('Saving TX...\n');
+
+      console.log('TX Saved!\n');
+      console.log('Button Test Completed!\n');
+
+      return;
+    }
+    
+    /* ============================================ */
+    /* ============================================ */
 
 }
-
 
 function write(item, toFile) {
   if (item && toFile) {
