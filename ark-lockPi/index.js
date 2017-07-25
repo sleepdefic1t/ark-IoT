ac5c8b884b3dec466f169f83a5529794bfb84ce4
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-25 10:08:25 -0400

    Create index.js

---
+++ ark-lockPi/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var figlet = require("figlet");
+var colors = require("colors");
+var vorpal = require('vorpal')();
+
+vorpal
+  .use(require('./ark-lock-pi.js'))
+  .show();
+
+vorpal.history('ark-lock');
+
+vorpal.log(colors.cyan(figlet.textSync("|||||||||||||||||||||||||||||", "digital")));
+vorpal.log(colors.cyan(figlet.textSync("|| Ark-Lock-Pi ||", "cybermedium")));
+vorpal.log(colors.cyan(figlet.textSync("|||||||| sleepdefIoT ||||||||", "digital")));
+
+vorpal
+  .delimiter('ark-lockPi>')
+  .show();
