c01072a39c3c0876409fffe6032b7a206dc3f596
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-25 14:21:50 -0400

    Create setup.js

---
+++ ark-lockPi/wake/setup.js
@@ -0,0 +1,12 @@
+var vorpal = require('vorpal')();
+
+vorpal
+  .use(require('../ark-lock-pi.js'))
+  .show();
+
+  var setup = function() {
+    vorpal.exec('lock setup');
+    vorpal.exec('exit');
+  };
+
+setup();
