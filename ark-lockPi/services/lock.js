c73c846f7123724ee13c0c92695cc43e47694e65
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-25 14:34:09 -0400

    Create lock.js

---
+++ ark-lockPi/services/lock.js
@@ -0,0 +1,101 @@
+var async = require("async");
+var events = require('events');
+var fs = require('fs');
+var Jar = require('./models/jar.js').Jar;
+// var rpio = require('rpio');
+var vorpal = require('vorpal')();
+
+module.exports = function (vorpal) {
+
+  /* ============================================ */
+  /* ================== Setup =================== */
+
+  var writeJSON = require('./services/storage.js');
+
+vorpal
+  .command('lock setup', "This will help you setup your lock")
+  .action(function(args, callback) {
+    var self = this;
+    async.waterfall([
+      function(seriesCb){
+        var price;
+        var lid;
+        var price;
+        self.prompt({
+          type: 'address',
+          name: 'address',
+          message: 'Ark Address: ',
+        }, function(result){
+          if (result.address) {
+            writeJSON(result.address, 'address');
+            self.log("**** Address was set! ****");
+            seriesCb(null, result.address);
+          } else {
+            self.log('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
+            seriesCb();
+          }
+        });
+      },
+      function(lid, seriesCb){
+        self.prompt({
+          type: 'lid',
+          name: 'lid',
+          message: 'LocationID: ',
+        }, function(result){
+          if (result.lid) {
+            writeJSON(result.lid, 'lid');
+            self.log("**** LocationID was set! ****");
+            seriesCb(null, result.lid);
+          } else {
+            self.log('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
+            seriesCb();
+          }
+        });
+      },
+      function(price, seriesCb){
+        self.prompt({
+          type: 'price',
+          name: 'price',
+          message: 'Cost of Entry: ',
+        }, function(result){
+          if (result.price) {
+            writeJSON(result.price, 'price');
+            console.log("**** Cost of Entry was set! ****");
+          } else {
+            seriesCb('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
+            seriesCb();
+          }
+        });
+      }
+    ], function(err){
+      if (err) throw err;
+      self.log("successful");
+      vorpal.exec('exit');
+      return callback();
+    });
+  });
+
+  /* ============================================ */
+  /* ============================================ */
+
+
+  /* ============================================ */
+  /* ========= Get Lock Info ========= */
+
+vorpal
+  .command('lock info', 'Read your address, lid, & price from file')
+  .action(function(args, callback, err) {
+    if (err) throw err;
+    fs.readFile('tmp/jar.json', function readFileCallback(err, data, callback){
+      if (err) throw err;
+      var parsedJar = JSON.parse(data);
+      var stringedJar = JSON.stringify(parsedJar, 'utf8', 2);
+      return console.log(stringedJar);
+    });
+    callback();
+  });
+
+  /* ============================================ */
+  /* ============================================ */
+
+}
