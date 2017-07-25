2b3fed3afef532a7c4a126efc3988e5d7a5cbf3b
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-25 14:56:56 -0400

    Update index.js

--- ark-lockPi/index.js
+++ ark-lockPi/index.js
@@ -5,7 +5,7 @@ var vorpal = require('vorpal')();
 
 vorpal
   .use(require('./ark-lock-pi.js'))
-  .use(require('.services/lock.js'))
+  .use(require('./services/lock.js'))
   .show();
 
 vorpal.history('ark-lock');
