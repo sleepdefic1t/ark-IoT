541ef451c8667a5fa2218677ba13c5284af1c1af
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-25 10:14:32 -0400

    Create pushButton.js

---
+++ ark-lockPi/onWake/pushButton.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var vorpal = require('vorpal')();
+
+vorpal
+  .use(require('../ark-lock-pi.js'))
+  .show();
+
+var pushButton = function() {
+  vorpal.exec('lock buttonPush');
+  vorpal.exec('exit');
+};
+
+pushButton();
