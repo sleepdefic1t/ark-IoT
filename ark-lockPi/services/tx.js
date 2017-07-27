fbe69d4341070cfa18087739d88c4d9353353ca5
Simon <sleepdefic1t@users.noreply.github.com>
2017-07-26 20:23:22 -0400

    Update tx.js

--- ark-lockPi/services/tx.js
+++ ark-lockPi/services/tx.js
@@ -1,3 +1,4 @@
+var ark = require('arkjs');
 var events = require('events');
 var fs = require('fs');
 var Jar = require('../models/jar.js').Jar;
@@ -7,103 +8,120 @@ var vorpal = require('vorpal')();
 
 var unlock = new events.EventEmitter();
 
+function dataNeedsMade() {
+  console.log('**** dataNeedsMade ****');
+  console.log("Making Smartbridge data...\n");
 
-module.exports = {
-
-  /* ============================================ */
-  /* ========= Create Smartbridge Data. ========= */
-  // use lid & prices & other to make smartbridge data
-
-  dataNeedsMade: function dataNeedsMade() {
-    console.log('**** dataNeedsMade ****');
-    console.log("Making Smartbridge data...\n");
-
-    fs.readFile('tmp/jar.json', function readFileCallback(err, data){
+  fs.readFile('tmp/jar.json', function readFileCallback(err, data){
+    if (err) throw err;
+    var newJar = JSON.parse(data, 'utf8', 2);
+    let ucData = 'sleepdefIoT' + "|" + newJar.lid + "|" + newJar.price;
+    // console.log(ucData);
+    // var hash = crypto.createHash('sha256');
+    // hash = hash.update(new Buffer(ucData,"utf-8")).digest();
+    fs.writeFile("tmp/data.dat", ucData, 'utf8', function (err) {
       if (err) throw err;
-      var newJar = JSON.parse(data, 'utf8', 2);
-      let ucData = 'sleepdefIoT' + "|" + newJar.lid + "|" + newJar.price;
-      // console.log(ucData);
-      // var hash = crypto.createHash('sha256');
-      // hash = hash.update(new Buffer(ucData,"utf-8")).digest();
-      fs.writeFile("tmp/data.dat", ucData, 'utf8', function (err) {
-        if (err) throw err;
-        return;
-      });
+      return;
     });
-    //if data was presented & scanned
-    var nfcWasRequested = require('./nfc.js').nfcWasRequested;
-    nfcWasRequested();
-  },
+  });
+  //if data was presented & scanned
+  var nfcWasRequested = require('./nfc.js').nfcWasRequested;
+  nfcWasRequested();
+}
 
-  /* ============================================ */
-  /* ============================================ */
+/* ============================================ */
+/* ============================================ */
+
+/* ============================================ */
+/* ================= Make  TX ================= */
 
 
-  /* ============================================ */
-  /* =============== Check for TX =============== */
-  // start checking for tx on mainnet
-  // check every X
-  // var checkTX = new events.EventEmitter();
+function shouldMakeTX() {
+  console.log('**** shouldMakeTX ****');
+  console.log("Making TX...\n")
 
-  shouldFindTX: function shouldFindTX() {
-    console.log('**** shouldFindTX ****');
-    console.log("Checking for TX...\n")
+  //   var amount      = 1.0 * Math.pow(10, 8); // 100000000000
+  // var transaction = ark.transaction.createTransaction("hxuG6XABWSN7swQ6Y8ner1CYHfTLeHLH6euB52fAtW6qRcbSfA", amount,null "passphrase", "secondPassphrase");
 
   // if (TXFound) {
-      var txWasFound = require('./tx').txWasFound;
-      return txWasFound();
+    var shouldFindTX = require('./tx').shouldFindTX;
+    return shouldFindTX();
   // } else {
   //   var shouldFindTX = require('./tx').shouldFindTX;
   //   return shouldFindTX();
   //   return;
   // }
-  },
+}
 
-  /* ============================================ */
-  /* ============================================ */
+/* ============================================ */
+/* ============================================ */
+
+/* ============================================ */
+/* =============== Check for TX =============== */
+// start checking for tx on mainnet
+// check every X
+// var checkTX = new events.EventEmitter();
+
+function shouldFindTX() {
+  console.log('**** shouldFindTX ****');
+  console.log("Checking for TX...\n")
+
+// if (TXFound) {
+    var txWasFound = require('./tx').txWasFound;
+    return txWasFound();
+// } else {
+//   var shouldFindTX = require('./tx').shouldFindTX;
+//   return shouldFindTX();
+//   return;
+// }
+}
+
+/* ============================================ */
+/* ============================================ */
 
 
-  /* ============================================ */
-  /* =============== Validate TX. =============== */
-  // Check found TX for validity before approving unlock
+/* ============================================ */
+/* =============== Validate TX. =============== */
+// Check found TX for validity before approving unlock
 
-  // var txReceived = new events.EventEmitter();
+// var txReceived = new events.EventEmitter();
 
-  txWasFound: function txWasFound() {
-    console.log('**** txWasFound ****');
-    console.log("Validating...\n");
+function txWasFound() {
+  console.log('**** txWasFound ****');
+  console.log("Validating...\n");
 
-    // if (TXValid) {
-    // UNLOCK
+  // if (TXValid) {
+  // UNLOCK
     var shouldUnlock = require('./unlock.js').shouldUnlock;
     shouldUnlock();
+    var shouldSaveTX = require('./tx').shouldSaveTX;
+    return shouldSaveTX();
+  // } else {
+  //   return;
+  // }
+}
 
+/* ============================================ */
+/* ============================================ */
 
-      var shouldSaveTX = require('./tx').shouldSaveTX;
-      return shouldSaveTX();
-    // } else {
-    //   return;
-    // }
-  },
+/* ============================================ */
+/* =============== Save TX Info =============== */
 
-  /* ============================================ */
-  /* ============================================ */
+//  save TX to file
+function shouldSaveTX() {
+  console.log('**** shouldSaveTX ****');
+  console.log('Saving TX...\n');
 
-  /* ============================================ */
-  /* =============== Save TX Info =============== */
+  console.log('TX Saved!\n');
+  console.log('Button Test Completed!\n');
 
-  //  save TX to file
-  shouldSaveTX: function shouldSaveTX() {
-    console.log('**** shouldSaveTX ****');
-    console.log('Saving TX...\n');
+  return;
+}
 
-    console.log('TX Saved!\n');
-    console.log('Button Test Completed!\n');
-
-    return;
-  }
-
-  /* ============================================ */
-  /* ============================================ */
-
-};
+module.exports = {
+  dataNeedsMade: dataNeedsMade,
+  shouldMakeTX: shouldMakeTX,
+  shouldFindTX: txWasFound,
+  txWasFound: txWasFound,
+  shouldSaveTX: shouldSaveTX
+}
