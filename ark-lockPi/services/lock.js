acb682ca2035cd468d46c1eb8dfcb50ada532bc1
Simon <sleepdefic1t@users.noreply.github.com>
2017-09-01 21:44:02 -0400

    Update lock.js

--- ark-lockPi/services/lock.js
+++ ark-lockPi/services/lock.js
@@ -1,111 +1,36 @@
-// #!/usr/bin/env node
-var async = require("async");
-var fs = require('fs');
-var Jar = require('../models/jar.js').Jar;
 // var rpio = require('rpio');
-var vorpal = require('vorpal')();
 
-module.exports = function (vorpal) {
+function needsUnlocked() {
+    console.log('\n **** lock.needsUnlocked ****');
+    console.log('   requesting NDEF Data... \n');    
+    nfc.dataWasRequested();  
+};
 
-  /* ============================================ */
-  /* ================== Setup =================== */
-  var writeJSON = require('./storage.js');
+function unlock() {
+    console.log('\n **** lock.unlock: Called **** \n');
+    /*
+    * Set the initial state to low.  The state is set prior to the pin becoming
+    * active, so is safe for devices which require a stable setup.
+    */
+    // rpio.open(12, rpio.OUTPUT, rpio.LOW);
 
-  vorpal
-    .command('lock setup', "This will help you setup your lock")
-    .action(function(args, callback) {
-      var self = this;
-      async.waterfall([
-        function(seriesCb){
-          var price;
-          var lid;
-          var price;
-          self.prompt({
-            type: 'address',
-            name: 'address',
-            message: 'Ark Address: ',
-          }, function(result){
-            if (result.address) {
-              writeJSON(result.address, 'address');
-              self.log("**** Address was set! ****");
-              seriesCb(null, result.address, result.lid);
-            } else {
-              self.log('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
-              seriesCb();
-            }
-          });
-        },
-        function(address, lid, seriesCb){
-          self.prompt({
-            type: 'lid',
-            name: 'lid',
-            message: 'LocationID: ',
-          }, function(result){
-            if (result.lid) {
-              writeJSON(result.lid, 'lid');
-              self.log("**** LocationID was set! ****");
-              seriesCb(null, result.lid);
-            } else {
-              self.log('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
-              seriesCb();
-            }
-          });
-        },
-        function(address, seriesCb){
-          self.prompt({
-            type: 'price',
-            name: 'price',
-            message: 'Cost of Entry: ',
-          }, function(result){
-            if (result.price) {
-              writeJSON(result.price, 'price');
-              console.log("**** Cost of Entry was set! ****");
-              seriesCb(null, address);
-            } else {
-              seriesCb('Entries must not be empty. For your security, you need to run the "setupLock" command again.');
-            }
-          });
-        }
-      ], function(err, result){
-        if (err) throw err;
-        self.log("successful");
-        callback();
-      });
-  })
+        // /* On for .5 seconds */
+        // rpio.write(12, rpio.HIGH);
+        // rpio.msleep(500);
 
-  /* ============================================ */
-  /* ============================================ */
+        // /* Off */
+        // rpio.write(12, rpio.LOW);
+        // rpio.msleep(500);
+        // wasUnlocked();
+
+};
+
+function wasUnlocked() {
+    console.log('\n **** lock.wasUnlocked: Called **** \n');
 
 
-  /* ============================================ */
-  /* ========= Get Lock Info ========= */
+};
 
-  vorpal
-    .command('lock info', 'Read your address, lid, & price from file')
-    .action(function(args, callback, err) {
-      if (err) throw err;
-      fs.readFile('tmp/jar.json', function readFileCallback(err, data, callback){
-        if (err) throw err;
-        var parsedJar = JSON.parse(data);
-        var stringedJar = JSON.stringify(parsedJar, 'utf8', 2);
-        return console.log(stringedJar);
-      });
-      callback();
-  })
-
-  /* ============================================ */
-  /* ============================================ */
-
-
-  /* ============================================ */
-  /* ============== Button Pushed. ============== */
-
-  vorpal
-    .command('lock pushButton', 'Simulates Button Push')
-    .action(function() {
-      console.log("Starting Button Test");
-      var buttonWasPushed = require('./unlock.js').buttonWasPushed;
-      buttonWasPushed();
-  })
-
-}
+module.exports = {
+    needsUnlocked: needsUnlocked
+};
