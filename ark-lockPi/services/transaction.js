7c3624359109dcc848812570bba305cee95356c4
Simon <sleepdefic1t@users.noreply.github.com>
2017-09-02 18:11:42 -0400

    Update transaction.js

--- ark-lockPi/services/transaction.js
+++ ark-lockPi/services/transaction.js
@@ -1,22 +1,23 @@
+var lock = require('./lock.js');
 var request = require('request');
 var storage = require('./storage.js');
 
+
 function findWith(hash) {
-    console.log('\n **** transaction.existsWith(hash): Called **** \n ..checking if transaction exists..');
+    console.log('\n **** transaction.findWith(hash): Called **** \n ..checking if transaction exists..');
     var address = String(storage.jarAddress());
     verifyTransactionsFor(address, hash);
 };
-
 function verifyTransactionsFor(address, hash) {
-    request('http://35.188.148.247:4002/api/transactions?recipientId=' + address,  function (error, response, body) {
+    request('http://35.188.148.247:4002/api/transactions?recipientId=' + address.substring(1, address.length-1),  function (error, response, body) {
         if (error || response.statusCode !== 200) {
             console.log('Transaction was not found.. \n');
-            waitFor(hash);
-        } else {
+            waitFor(hash);            
+        } else {            
             checkRequest(body, hash);
         }
     });    
-}
+};
 
 function checkRequest(body, hash) {
     console.log('\n Checking Transaction Records.. \n');
@@ -26,9 +27,9 @@ function checkRequest(body, hash) {
     for (var index in allTransactionsOfLock) {                    
         vendorFields.push(allTransactionsOfLock[index].vendorField);
         confirmations.push(allTransactionsOfLock[index].confirmations);
-    };
-    var indexOfHash = vendorFields.indexOf(hash);
-    // var indexOfHash = vendorFields.indexOf("║▌║█║▌  Ѧ  ║▌║▌█");        // testHash     
+    };    
+    // var indexOfHash = vendorFields.indexOf(hash);
+    var indexOfHash = vendorFields.indexOf("║▌║█║▌  Ѧ  ║▌║▌█");        // testHash     
     if (indexOfHash > -1 && confirmations[indexOfHash] > 3) {     
         console.log('\n Transaction Found! Starting unlock sequence.. \n');
         wasFound();
@@ -36,7 +37,7 @@ function checkRequest(body, hash) {
         console.log('Transaction was not found.. \n ...trying again in 5 seconds. \n'); 
         waitFor(hash);                
     }
-}
+};
 
 function waitFor(hash) {
     setTimeout(function() {
@@ -46,6 +47,7 @@ function waitFor(hash) {
 
 function wasFound() {
     console.log('\n ***transaction.wasFound: Called** \n');
+    lock.needsUnlocked();
 };
 
 module.exports = {
