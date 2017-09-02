875a668b57ca88a94322e2a391be43fed91a7fa5
Simon <sleepdefic1t@users.noreply.github.com>
2017-09-01 21:47:28 -0400

    Update storage.js

--- ark-lockPi/services/storage.js
+++ ark-lockPi/services/storage.js
@@ -1,31 +1,70 @@
 var fs = require('fs');
-var Jar = require('../models/jar.js').Jar;
 
 /* ============================================ */
 /* =============== Data Storage =============== */
 
-var writeJSON = function writeJSON(item, toKey) {
-  var self = this;
-  fs.open('tmp/jar.json', 'r', (err) => {
-    if (err) throw err;
-    fs.readFile('tmp/jar.json', function readFileCallback(err, data){
-      if (err) throw err;
-      var newJar = JSON.parse(data, 'utf8', 2);
-      switch (true) {
-        case (toKey == 'address'): newJar.address = item; break;
-        case (toKey == 'lid'): newJar.lid = item; break;
-        case (toKey == 'price'): newJar.price = item; break;
-        case (toKey == 'receipts'):  newJar.receipts += [item];  break;
-      default:
-        break;
-      }
-      var newestJar = JSON.stringify(newJar, 'utf8', 2);
-      fs.writeFile("tmp/jar.json", newestJar, function (err) {
-        if (err) throw err;
-        // return;
-      });
+function writeJSON(item, toKey) {
+    console.log('\n **** storage.writeJSON: Called **** \n');
+    fs.open('tmp/jar.json', 'r', (err) => {
+        if(err) throw err;
+        fs.readFile('tmp/jar.json', function readFileCallback(err, data) {
+            if (err) throw err;
+            var newJar = JSON.parse(data, 'utf8', 2);
+            switch (true) {
+                case (toKey === 'address'):
+                    newJar.address = item;
+                    break;
+                case (toKey === 'lid'):
+                    newJar.lid = item;
+                    break;
+                case (toKey === 'price'):
+                    newJar.price = item;
+                    break;
+                default:
+                    break;
+            }
+            var newestJar = JSON.stringify(newJar, 'utf8', 2);
+            fs.writeFile("tmp/jar.json", newestJar, function (err) {
+                if (err) throw err;
+            });
+        });
     });
-  });
 };
 
-module.exports = writeJSON;
+function stringifiedJar() {
+    var jar = fs.readFileSync('tmp/jar.json', 'utf8');    
+    var parseJar = JSON.parse(jar);
+    return JSON.stringify(parseJar, 'utf8')
+};
+
+function jarAddress() {
+    var jar = fs.readFileSync('tmp/jar.json', 'utf8');    
+    var parseJar = JSON.parse(jar);
+    return JSON.stringify(parseJar.address);
+};
+
+function addReceipt(message, hash) {
+    console.log('\n **** storage.addReceipt: Called **** \n');
+    console.log('   Saving receipt info..');      
+    fs.open('tmp/receipts.dat', 'r', (err) => {
+        if(err) throw err;
+        fs.readFile('tmp/receipts.dat', function readFileCallback(err, data) {
+            if (err) throw err;
+            var oldData = data;
+            var newMessage = 'message: ' + message + ' | hash: ' + hash;
+            var newData = oldData + '\n\n' + newMessage;
+            fs.writeFile('tmp/receipts.dat', newData, function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+    console.log('   Receipt info saved. \n');      
+};
+
+module.exports = {
+    addReceipt: addReceipt,
+    jarAddress: jarAddress,
+    stringifiedJar: stringifiedJar,
+    // writeDataDat: writeDataDat,
+    writeJSON: writeJSON
+}
