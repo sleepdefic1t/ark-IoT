ca3faee24e78d4038a383f680561ed3d8e1a4308
Simon <sleepdefic1t@users.noreply.github.com>
2017-09-02 18:20:09 -0400

    Update lock.js

--- ark-lockPi/services/lock.js
+++ ark-lockPi/services/lock.js
@@ -1,34 +1,16 @@
-// var rpio = require('rpio');
+var rpio = require('rpio');
 
 function needsUnlocked() {
-    console.log('\n **** lock.needsUnlocked ****');
-    console.log('   requesting NDEF Data... \n');    
-    nfc.dataWasRequested();  
-};
-
-function unlock() {
     console.log('\n **** lock.unlock: Called **** \n');
-    /*
-    * Set the initial state to low.  The state is set prior to the pin becoming
-    * active, so is safe for devices which require a stable setup.
-    */
-    // rpio.open(12, rpio.OUTPUT, rpio.LOW);
-
-        // /* On for .5 seconds */
-        // rpio.write(12, rpio.HIGH);
-        // rpio.msleep(500);
-
-        // /* Off */
-        // rpio.write(12, rpio.LOW);
-        // rpio.msleep(500);
-        // wasUnlocked();
-
+    rpio.open(12, rpio.OUTPUT, rpio.LOW);
+    rpio.write(12, rpio.HIGH); /* On */
+    rpio.msleep(500);          /* Sleep for .5 seconds */
+    rpio.write(12, rpio.LOW);  /* Off */
+    wasUnlocked();
 };
 
 function wasUnlocked() {
     console.log('\n **** lock.wasUnlocked: Called **** \n');
-
-
 };
 
 module.exports = {
