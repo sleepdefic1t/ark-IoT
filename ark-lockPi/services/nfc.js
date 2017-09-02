98e257d5486dabeab5b943b4287f995727dd01ce
Simon <sleepdefic1t@users.noreply.github.com>
2017-09-02 18:15:18 -0400

    Update nfc.js

--- ark-lockPi/services/nfc.js
+++ ark-lockPi/services/nfc.js
@@ -3,14 +3,13 @@ var rpio = require('rpio');
 var transaction = require('./transaction.js');
 var network = require('./network.js');
 
-
 function dataWasRequested() {
   console.log('\n **** nfc.dataWasRequested ****');
   console.log('   making NDEF Data... \n');    
   if (network.isConnectable() === true) {
     var nfcData = data.forNFC();
     console.log('\n *NDEF Data made. Ready to present* \n');  
-    powerOnNFC();
+    powerOn();    
     presentNDEFWith(nfcData);
   };
 };
@@ -18,33 +17,33 @@ function dataWasRequested() {
 function presentNDEFWith(nfcData) {
   console.log('\n **** nfc.presentNDEFWith|nfcData: Called ****');
   console.log('   presenting NDEF Data via NFC... \n');      
+  // send ndef
   post(nfcData);
 };
 
 function post(nfcData){
   console.log('\n **** nfc.post|nfcData: Called **** \n');
+
   // if okay, continue
   console.log('\n *NDEF Data was shared... Notifying system..* \n');      
   wasPostedWith(nfcData);
-}
-
+};
 
 function powerOn() {
   console.log('\n **** nfc.powerOn: Called **** \n');
-    rpio.open(2, rpio.OUTPUT, rpio.LOW);
-    /* On */
-    rpio.write(24, rpio.HIGH);
+  rpio.open(2, rpio.OUTPUT, rpio.LOW);
+  rpio.write(24, rpio.HIGH); /* On */
 };
+
 function powerOff() {
   console.log('\n **** nfc.powerOff: Called **** \n');  
-    /* Off */
-    rpio.write(2, rpio.LOW);
-    wasUnlocked();
+  rpio.write(2, rpio.LOW); /* Off */
 };
 
 function wasPostedWith(nfcData) {    
   console.log('\n **** nfc.wasPostedWith|data ****');
   console.log('   preparing to check for transaction confirmation.. \n');      
+  powerOff();
   transaction.findWith(nfcData.hash);
 };
 
